24ab1383eb6bc832a220b8a2f7511fd00ac26cc7
IsabelNavarrete04 <123046270@upq.edu.mx>
2025-02-04 14:17:21 -0600

    Termine el primer ejercicio del examen; múltiplos de 10 comprendidos entre 10 y 1000.

---
+++ Examen_1/src/Examen1_Ejercicio1.java
@@ -0,0 +1,8 @@
+public class Examen1_Ejercicio1{
+       public static void main(String[] args){
+         int i;
+         for(i=10; i<=1000; i+=10){
+             System.out.println(i);
+         }
+      }
+}
