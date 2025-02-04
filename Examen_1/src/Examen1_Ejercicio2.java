a4fb456f962786b67de150aadd8416447a800b75
IsabelNavarrete04 <123046270@upq.edu.mx>
2025-02-04 14:50:06 -0600

    Voy a la mitad del segundo ejercicio

---
+++ Examen_1/src/Examen1_Ejercicio2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Examen1_Ejercicio2 {
+    public static void main(String[] args) {
+        Scanner cadena = new Scanner(System.in);
+        System.out.println("Ingresa una palabra: ");
+        String palabra1 = cadena.nextLine();
+        System.out.println("Ingresa otra palabra: ");
+        String palabra2 = cadena.nextLine();
+
+        if(palabra1.length()>= palabra2.length()){
+             System.out.println(palabra1 + " es mas larga");
+        }else{
+            System.out.println(palabra2 + " es mas larga");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
